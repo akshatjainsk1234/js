cf7d38693a115b886d6ac1f687507d10b9dc44f6
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-12-11 17:56:13 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,9 @@
+console.log(typeof "akshat")
+// number
+// string
+// booleon
+// undefined 
+// null     these all are primitive datatyes
+
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01basics/variable.js
@@ -0,0 +1,10 @@
+const accountID=456231
+let email="akshat@gmail.com"
+var password="12345"
+accountcity="kosi"
+
+
+
+// console.log(accountID)
+
+console.table([accountID,email,password,accountcity])
